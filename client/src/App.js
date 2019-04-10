893db4684cb11453435a8c7f50aa3646ed6238df
Stephen Thomas <stevie.t.siren@gmail.com>
2019-04-09 20:31:30 -0700

    Added register form

--- client/src/App.js
+++ client/src/App.js
@@ -1,19 +1,29 @@
 import React, { Component } from 'react';
+
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+
 import './App.css';
 import './css/styles.css';
 
 import Navbar from './components/layout/Navbar';
 import Footer from './components/layout/Footer';
 import Landing from './components/layout/Landing';
+import Register from './components/auth/Register';
+import Login from './components/auth/Login';
 
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <Navbar />
-        <Landing />
-      </div>
+      <Router>
+        <div className="App">
+          <Navbar />
+            <Route exact path="/" component={ Landing } />
+            <Route exact path="/register" component={ Register } />
+            <Route exact path="/login" component={Login} />
+          <Footer />
+        </div>
+      </Router>
     );
   }
 }
--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -1,26 +1,21 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 
 class Navbar extends Component {
   render() {
     return (
         <nav className="navbar navbar-expand-lg navbar-light bg-light">
-            <a className="navbar-brand" href="#">Navbar</a>
+            <Link className="navbar-brand" to="/">Navbar</Link>
             <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
             <span className="navbar-toggler-icon"></span>
             </button>
             <div className="collapse navbar-collapse" id="navbarNav">
             <ul className="navbar-nav">
                 <li className="nav-item active">
-                <a className="nav-link" href="#">Home <span className="sr-only">(current)</span></a>
+                  <Link className="nav-link" to="/login">Login</Link>
                 </li>
                 <li className="nav-item">
-                <a className="nav-link" href="#">Features</a>
-                </li>
-                <li className="nav-item">
-                <a className="nav-link" href="#">Pricing</a>
-                </li>
-                <li className="nav-item">
-                <a className="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
+                  <Link className="nav-link" to="/register">Register</Link>
                 </li>
             </ul>
             </div>
